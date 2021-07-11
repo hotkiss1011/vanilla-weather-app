faf9e5a7bb9f845c3553a9cea0fd7c2cd7573631
hotkiss1011 <amandapandah@yahoo.com>
2021-07-10 20:05:57 -0400

    added Celsius/Fahrenheit conversion
    site is now able to display Celsius or Fahrenheit

--- src/app.js
+++ src/app.js
@@ -1,63 +1,103 @@
 function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes =`0${minutes}`;
-    }
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemp(response) {
-    console.log(response.data);
-    let tempElement = document.querySelector("#temp");
-    tempElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
 
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.name;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
 
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
-    let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  fTemp = Math.round(response.data.main.temp);
 }
 
-  function search(city) {
+function search(city) {
     let key = "05992a658e151609dfa497fc6c2796f2";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
 
-    axios.get(apiUrl).then(displayTemp);
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
-    console.log(cityInputElement.value);
-  }
-  
-  let form = document.querySelector("#search-form");
-  form.addEventListener("submit", handleSubmit);
+  axios.get(apiUrl).then(displayTemp);
+}
 
-  search("New York")
+function handleSubmit(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+
+  document.getElementById("search-form").value = '';
+}
+
+function showCelsius(event) {
+    event.preventDefault();
+
+    let tempElement = document.querySelector("#temp");
+
+    fahrenheitLink.classList.remove("active");
+    celsiusLink.classList.add("active");
+
+    let celsiusTemp = Math.round((fTemp * 9 / 5) + 32);
+    tempElement.innerHTML = celsiusTemp;  
+}
+
+function showFahrenheit(event) {
+  event.preventDefault();
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = fTemp;
+}
+
+let fTemp = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsius);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+search("Knoxville");
